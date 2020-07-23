00e418ddda17592f87956fb586f8cf884b4b5f37
rdelarosa3 <rdelarosa39@gmail.com>
2020-07-23 16:25:08 -0500

    html + js lecture complete

---
+++ js/external-example.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("external works");
+
+// alert("External Works")
+//
+// var response = confirm("Is it Raining?");
+// console.log("response", response);
+
+var name = prompt("Whats your name?");
+console.log(typeof name);
+console.log("name:",name);
+console.log("length:", name.length)
