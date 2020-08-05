c3086369f7a0012bd589f4f59607695745a69e92
Robert De la Rosa <rdelarosa39@gmail.com>
2020-08-05 13:00:23 -0500

    updated: lab exercise day done

--- js/labs.js
+++ js/labs.js
@@ -1,28 +1,66 @@
 var storeGoods = [];
-var productName = prompt("What is the product name?");
-var productType = prompt("What is the product Type?");
-var goodPrice = prompt("What is the product Price?")
-
-
+function getName(){
+    let productName = prompt("What is the product name?");
+    return typeof productName === "string" ? productName : getName();
+}
+function getType() {
+    let productType = prompt("What is the product Type?");
+    return typeof productType === "string" ? productType : getType();
+}
+// Ensure price is a number
+function getPrice() {
+    let productPrice = prompt("What is the product Price?");
+    if(typeof productPrice ==="boolean"|| typeof productPrice == null){
+        return getPrice();
+    }else if(isNaN( parseFloat(productPrice))){
+        return getPrice();
+    }
+    return parseFloat(productPrice);
+}
+// Check if item is tax exempt
 function isTaxExempt(typeOfGood){
     let exemptGoods = ["book","food","medical"];
     return exemptGoods.includes(typeOfGood);
 }
+// create products for the store
 function createProduct(goodName,goodType, goodPrice){
     let isImport = confirm("Is good Imported?");
     let isExempt = isTaxExempt(goodType);
-    storeGoods[goodName]= {
-        productType: goodType,
-        productImported: isImport,
-        productPrice: goodPrice
-
-    }
+    let salesTax = .1
     if(isImport){
-        storeGoods[goodName][productTax] = .15;
+        salesTax = .15;
     }else if(isExempt){
-        storeGoods[goodName][productTax] = 0;
-    }else{
-        storeGoods[goodName][productTax] = .1
+        salesTax = 0;
     }
+    storeGoods.push(
+        {
+            productName: goodName,
+            productType: goodType,
+            productImported: isImport,
+            productPrice: goodPrice,
+            productTax: salesTax
+        }
+    )
+}
+// Sales Calculator
+function totalCalc(cart) {
+    var subtotal = 0;
+    var salesTax = 0;
+    cart.forEach(function(good) {
+        console.log(good);
+        let tax = (good.productPrice * good.productTax);
+        salesTax += tax;
+        subtotal += (good.productPrice);
+    })
+    console.log(subtotal.toFixed(2));
+    console.log(salesTax.toFixed(2));
+    console.log((subtotal+salesTax).toFixed(2));
+    return (subtotal+salesTax).toFixed(2);
 }
 
+// Create sample store products
+createProduct("book1","book",12.49);
+createProduct("music cd","music",14.99);
+createProduct("chocolate","food",.85)
+// calculate total of products in store
+totalCalc(storeGoods);
